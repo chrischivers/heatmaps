a0254cde3f506752b18e1aa196443d4c8c9b6940
Chris Chivers <chris.chivers@itv.com>
2017-07-29 23:39:42 +0100

    appending file occurs on completion of db insert

--- src/main/scala/heatmaps/ScannerApp.scala
+++ src/main/scala/heatmaps/ScannerApp.scala
@@ -5,10 +5,11 @@ import java.io.{File, FileOutputStream, PrintWriter}
 import com.typesafe.scalalogging.StrictLogging
 import heatmaps.db.{PlaceTableSchema, PostgresqlDB}
 
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 import scala.concurrent.{Await, Future}
-import scala.concurrent.ExecutionContext.Implicits.global
 import scala.io.Source
+import scala.util.{Failure, Success}
 
 object ScannerApp extends App with StrictLogging {
   val config = ConfigLoader.defaultConfig
@@ -22,6 +23,12 @@ object ScannerApp extends App with StrictLogging {
     LatLngRegion(splitLine(0).toInt, splitLine(1).toInt)
   }).toList
 
+  def appendLatLngToFile(latLngRegion: LatLngRegion) = {
+    val writer = new PrintWriter(new FileOutputStream(new File("regionsscanned.txt"),true))
+    writer.append("\n" + latLngRegion.toString)
+    writer.close()
+  }
+
   val result: Future[List[Unit]] = Future.sequence {
     val allRegions = Definitions.latLngRegions
     val regionsToScan = allRegions.filterNot(region => regionsAlreadyScanned.contains(region))
@@ -34,12 +41,17 @@ object ScannerApp extends App with StrictLogging {
       val result = Definitions.placeTypes.map { placeType =>
         logger.info(s"Scanning placeType $placeType")
         val scanResults = ls.scanForPlacesInLatLngRegion(latLngRegion, 10000, placeType)
-        db.insertPlaces(scanResults, latLngRegion, placeType)
-      }
-      val writer = new PrintWriter(new FileOutputStream(new File("regionsscanned.txt"),true))
-      writer.append("\n" + latLngRegion.toString)
-      writer.close()
+        val dbInsertResult = db.insertPlaces(scanResults, latLngRegion, placeType)
 
+        dbInsertResult.onComplete {
+          case Success(_) =>
+            appendLatLngToFile(latLngRegion)
+          case Failure(e) =>
+            logger.error(s"Error writing $latLngRegion to DB", e)
+            throw e
+        }
+        dbInsertResult
+      }
       result
     }
   }
